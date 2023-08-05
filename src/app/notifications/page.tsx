a50e6ee66b45b6d0801038de46b2b9a36770ecac
Ridho Febriansa <officialridhof@gmail.com>
2023-08-05 11:50:56 +0000

    feat: notification sample ui

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -2,11 +2,21 @@
 
 import { BottomNav, UpperNav } from '../components'
 import { AppPageLayout } from '../containers'
+import { Card, CardBody, Stack, StackDivider, Text } from '@chakra-ui/react'
 
 export default function Notifications() {
     return (
         <AppPageLayout>
             <UpperNav title="Notifications" />
+            <Card>
+               <CardBody>
+                    <Stack divider={<StackDivider />}>
+                        <Text>Adam invited you to Cinema Collection</Text>
+                        <Text>Bill has set Baseball Collection to public</Text>
+                        <Text>Clinton has added a new bookmark to Basketball News</Text>
+                    </Stack>
+                </CardBody> 
+            </Card>
             <BottomNav />
         </AppPageLayout>
     )
