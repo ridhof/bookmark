c6272ade56ca6c0713df77c95286657685f4d838
Ridho Febriansa <officialridhof@gmail.com>
2023-08-11 23:50:13 +0000

    chore: add sample vercek kv redis usage

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,4 @@
+import { kv } from "@vercel/kv";
 import { sql } from "@vercel/postgres";
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
@@ -6,6 +7,12 @@ export async function GET(request: NextRequest) {
     try {
         const { rows } = await sql`SELECT * FROM _prisma_migrations LIMIT 1;`;
         console.log(rows);
+        const initialGetKV = await kv.get<{ message: string }[]>("test-bookmark");
+        console.log("initial kv: " + initialGetKV);
+        const setKV = await kv.set("test-bookmark", { message: "initial kv setup" }, { ex: 60 * 15 });
+        console.log("set kv: " + setKV);
+        const getKV = await kv.get<{ message: string }[]>("test-bookmark");
+        console.log("get kv: " + getKV);
         return NextResponse.json(
             {
                 body: request.body,
@@ -13,6 +20,9 @@ export async function GET(request: NextRequest) {
                 query: request.nextUrl.search,
                 cookies: request.cookies.getAll(),
                 rows: rows,
+                initial_kv: initialGetKV,
+                set_kv: setKV,
+                get_kv: getKV,
             },
             {
                 status: 200,
