395753d7fca60c6830abaf31332663b5595ba2ed
Ridho Febriansa <officialridhof@gmail.com>
2023-08-11 22:20:17 +0000

    chore: sample usage of prisma migration and vercel postgre db query

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,20 +1,25 @@
+import { sql } from "@vercel/postgres";
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
     try {
+        const { rows } = await sql`SELECT * FROM _prisma_migrations LIMIT 1;`;
+        console.log(rows);
         return NextResponse.json(
             {
                 body: request.body,
                 path: request.nextUrl.pathname,
                 query: request.nextUrl.search,
                 cookies: request.cookies.getAll(),
+                rows: rows,
             },
             {
                 status: 200,
             },
         );
     } catch (err) {
+        console.log("err: " + err);
         return NextResponse.json(
             {},
             { status: 400 }
--- supabase/migrations/20230811080004_create_table_newsletter_users.sql
+++
@@ -1,8 +0,0 @@
-create table newsletter_users (
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    email varchar(254) NOT NULL UNIQUE,
-    otp varchar(6),
-    is_verified boolean DEFAULT false,
-    created_at timestamp DEFAULT current_timestamp,
-    modified_at timestamp DEFAULT current_timestamp
-);
